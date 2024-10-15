d7be2f683dac7d8f3c9d14296424d7df812a794b
Laura Bussi <laura.bussi@isti.cnr.it>
2024-10-15 12:30:27 +0000

    remove some comments

--- scripts/toolchain.py
+++ scripts/toolchain.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
 #%%
-# converting to lps...
-# ^B^BSegmentation fault (core dumped)
-# mcrl2lps time: 14168.65964460373
-
-# TODO: NECESSARY: switch to an execution function that accepts an array of arguments
 
 print("started")
 
 
 import json
 import subprocess
-# import sys
 import regex as re
 import os
 import time
@@ -35,24 +29,12 @@ def run_command(command):
     print(f"Running command: {command}")
     subprocess.run(command, shell=True)
 
-# def init_time():
-#     global last_time
-#     last_time = time.time()
-
-# def update_time():
-#     global last_time
-#     tmp = last_time
-#     last_time = time.time()
-#     return last_time - tmp
-
-
 times = ({})
 
 def loadData(args):
     with open(args["poset"]) as f:
         # Load the JSON data into a dictionary
         args["data"] = json.load(f)
-        # print(len(args["data"]["points"]))
 
 
 def poset2mcrl2(args):
@@ -82,8 +64,6 @@ def poset2mcrl2(args):
 
     def encode(uncovered):
         print("interpreting data...")
-        # Mieke added sorting of LTS states in next line  (2023/06/07)
-        # VC: why?
         dg = nx.DiGraph(uncovered["up"])
         tc = nx.transitive_closure(dg)
 
@@ -105,14 +85,8 @@ def poset2mcrl2(args):
             atoms.add(stlabel)
 
         for (point, dest) in tc.edges():
-            # MKE: here one could first check whether point and dest are just one step in face relation apart
-            # and only in that case add the tau-transitions
-            # VINCENZO: is this what is done by the following if?
-            # VINCENZO: could this be done separately on the non-transitively-closed graph for efficiency?
             if v[dest] == v[point]:
-                # The following line is only justified if it is sure that the
-                # point is in a monochromatic upset!!!
-                # TODO: URGENT: VC: I do not understand this line at all
+                # Optimisation for points that are in a monochromatic upset
                 if optimise == 1: 
                     if dg.has_edge(point, dest):
                         label = "tau"
@@ -156,7 +130,6 @@ def poset2mcrl2(args):
         return act != "tau"
 
     with open(f"{output_dir}/{base_name}.mcrl2", "w") as outfile:
-        # fn=relation_to_function(encoded["transitions"])
         outfile.write("act\n    ")
 
         for a in intersperse(filter(notau, encoded["atoms"]), ","):
@@ -181,20 +154,17 @@ def poset2mcrl2(args):
             for x in ts1:
                 outfile.write(x)
             outfile.write(";")
-        # VC: TODO: URGENT: is the initial state always 0?
         outfile.write(f"\n\ninit\n\n{name_state(0)};")
     return encoded
 
 
-def poly2poset(args): #TODO: switch to the release version of PolyPo (not Debug) and also compile it if not present
+def poly2poset(args):
     run_command(f"../../scripts/PolyPoProject/bin/Release/net8.0/PolyPoProject " +
                 args["poly"] + " " + args["poset"])
 
 
 def mcrl2lps(args):
     print("converting to lps...")
-    #run_command(f"mcrl22lps -rjittyc --no-alpha --no-cluster --no-constelm --no-deltaelm --no-globvars --no-rewrite --no-sumelm " +
-    #            args["mcrl2"] + " " + args["lps"])
     run_command(f"mcrl22lps --no-alpha --no-cluster --no-constelm --no-deltaelm --no-globvars --no-rewrite --no-sumelm " +
                 args["mcrl2"] + " " + args["lps"])
 
@@ -224,26 +194,17 @@ def findStates(args):
                 # convert the final part of the string to int and place it in the list of states:
                 states[int(num_str)] = int(re.findall(r'\d+', next_no_whitespace)[-1])
     args["states"] = states
-    #with open("statesFile.txt", 'w') as s:
-    #    json.dump(states,s)
 
 
 def lps2lts(args):
     print("converting to lts...")
-    # run_command("lps2lts  --cached -rjittyc --threads=32 " +
-    #             args["lps"] + " " + args["lts"])
     run_command("lps2lts  --cached --threads=32 " +
                 args["lps"] + " " + args["lts"])
-    # run_command("ltsconvert " + args["lts"] + " " + args["lts"] + ".dot")
-    # run_command("neato -Tpdf " + args["lts"] + ".dot" + " -o " + args["lts"] + ".pdf" )
 
 
 def ltsminimise(args):
     print("minimizing....")
-    #TODO: please use a variable to hold the filenames, and use popen with an array of arguments instead of run_command
     run_command("ltsconvert -ebranching-bisim " + args["base"] + " " + args["minimised"])    
-    # run_command("ltsconvert " + args["minimised"] + " " + args["minimised"] + ".dot")
-    # run_command("neato -Tpdf " + args["minimised"] + ".dot" + " -o " + args["minimised"] + ".pdf" )
 
 
 def createJsonFiles(args):
@@ -251,9 +212,6 @@ def createJsonFiles(args):
     result = subprocess.run(
         "ltsinfo -l " + args["minimised"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stderr_str = result.stderr
-    # parse ltsinfo.txt and decode it
-    #with open("ltsinfo.txt", 'wb') as infofile:
-    #    infofile.write(stderr_str)
     decoded = stderr_str.decode("utf-8")
 
     pairs = [(0, 0) for i in range(0, points)]
@@ -277,8 +235,6 @@ def createJsonFiles(args):
         # now assign classes to the original states
         index = states.index(int(splitted[1]))
         pairs[index] = (int(splitted[0]), index)
-    # print(states)
-    # print(pairs)
 
     # get the correspondence between original states and atoms (don't know if this is needed)
     color_state = [(0, "") for i in range(0, points)]
@@ -297,19 +253,13 @@ def createJsonFiles(args):
     jsonDict =  {}
     for i in range(0, len(classes)):
         jsonDict["class" + str(classes[i])] = []
-    # jsonArrays = [{"class" + str(classes[i]): []} for i in range(0, len(classes))]
 
-    # for i in range(0, len(classes)):
         for j in range(0, len(pairs)):
             if classes[i] == pairs[j][0]:
                 jsonDict["class" + str(classes[i])].append(True)
             else:
                 jsonDict["class" + str(classes[i])].append(False)
-            # with open(args["output"] + "/jsonOutput" + str(classes[i]) + ".json", 'w') as outjson:
-            #     json.dump(jsonArrays[i], outjson, indent=2)
 
-    # for i in range(0, len(classes)):
-    #     jsonDict["class" + str(i)] = jsonArrays[i].values
     with open(args["output"] + "/jsonOutputAll.json", 'w') as outjson:
                 json.dump(jsonDict, outjson, indent=2)
 
@@ -327,21 +277,14 @@ def createModelFiles(args):
         match = re.match(r'.*\((\d+),("dwn"|"ap.*"),(\d+)\)', el)
         l =[]
         if match:
-            #print(match.group(0))
             dest = match.group(1) # this is the destination of an "up" transition
             atom = match.group(2)
             source = match.group(3)
-            # if(source == "19"):
-            #     print("el is ", el, dest, atom)  #+ " " + match.group(2))
             if not source in tmpDict:
-                #if(source=="19"):
-                #    print("then")
                 outDict["points"].append({ "id": source, "up": [], "atoms": []})
                 tmpDict[source] = len(outDict["points"]) - 1
                 outDict["points"][tmpDict[source]]["up"].append(dest)
             else:
-                #if(source=="19"):
-                #    print("else")
                 if not dest in outDict["points"][tmpDict[source]]["up"]:
                     outDict["points"][tmpDict[source]]["up"].append(dest)
             if "ap" in atom:
@@ -353,7 +296,7 @@ def createModelFiles(args):
 
 # CACHED EXECUTION
 
-use_cache = True #TODO: make this a command line switch, default to True
+use_cache = True
 
 def cached_execute(filename, name, fn, args):
     start_time = time.time()
@@ -373,11 +316,7 @@ args = cli.parse_args()
 
 optimise = args.optimise
 
-input_file = args.file  # this is base_name.json
-
-# For interactive use:
-# os.chdir("/workspaces/case-study-eta-minimization/experiments/triangle")
-# input_file = "triangleRBModel_Poset.json"
+input_file = args.file  
 
 base_name = os.path.splitext(input_file)[0]
 
@@ -410,7 +349,6 @@ x = cached_execute(output_dir + "/" + base_name + ".mcrl2", f"poset2mcrl2",
 def debug():
     return nx.to_dict_of_dicts(x["lts"])
 
-#debug()
 #%%
 
 # generate the lps and get the lps pretty print
